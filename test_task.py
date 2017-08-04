75dbd6ca04da73c88049b5ef0fa0635362bfaf1e
NewAgeQuanta <newagequanta@gmail.com>
2017-08-04 16:01:43 -0400

    bug in test_task

--- test_task.py
+++ test_task.py
@@ -1,10 +1,11 @@
 """
 pytest unit tests
-Functions should start with ``test_`` and contain a list of assertions that evaluate to True if the test succeeds.
+Functions should start with ``test_`` and contain a list of assertions that
+evaluate to True if the test succeeds.
 For more info see: http://pytest.org/latest/
 """
 
 import io
-import task_bug_scrub_csv_diff
+import task_bug_scrub_csv_diff as bs
 def test_open_text_file():
-    assert type(task_bug_scrub_csv_diff('test_file.txt')) == io.TextIOWrapper
+    assert type(bs.open_text_file('test_file.txt')) == io.TextIOWrapper
