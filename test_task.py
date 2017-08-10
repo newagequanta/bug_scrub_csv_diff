fb16dd85e9f61b622cbe7068cc3a70fe3662b98e
NewAgeQuanta <newagequanta@gmail.com>
2017-08-10 11:43:50 -0400

    test function with sha256 hash compariston

--- test_task.py
+++ test_task.py
@@ -4,12 +4,29 @@ Functions should start with ``test_`` and contain a list of assertions that
 evaluate to True if the test succeeds.
 For more info see: http://pytest.org/latest/
 """
+import sys
+import task_bug_scrub_csv_diff
+import hashlib
 
-import io
-import task_bug_scrub_csv_diff as bs
+sys.path = ['', '/venv/python361-1/lib/python36.zip', '/venv/python361-1/lib/python3.6',
+            '/venv/python361-1/lib/python3.6/lib-dynload',
+            '/home/bdb/.pyenv/versions/3.6.1/lib/python3.6',
+            '/venv/python361-1/lib/python3.6/site-packages',
+            '/venv/python361-1/src/jtextfsm']
+
+
 def test_task():
-    return_text = ('Task Finished.\n'
-                   'records_added.csv and records_deleted.csv created.\n'
-                   'Please download them from your files.')
-    assert bs.task(None, 'GDC_852.csv', 'GDC_666.csv') == return_text
-
+    '''
+    test the hashes of the files created against precalculated hashes
+    '''
+    task_bug_scrub_csv_diff.task(None,
+                                 '/var/bdb/sessions/sandboxed-user/test_852.csv',
+                                 '/var/bdb/sessions/sandboxed-user/test_666.csv')
+    hash_added = hashlib.sha256(open('records_added.csv',
+                                     'rb').read()).hexdigest()
+    hash_deleted = hashlib.sha256(open('records_deleted.csv',
+                                       'rb').read()).hexdigest()
+    hash_total = hash_added + hash_deleted
+    assert hash_total == (
+        '84217c0024d9966b25c8ec4988b457bfbc58af02381188af95479d702ab45b24' +
+        '17db7b957ebba1e8e7d3812a9f349e43eb4da0d0da053fb773e42162f1e9d30f')
